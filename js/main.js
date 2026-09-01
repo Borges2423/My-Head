6ed1a278b23670e1498fd333460ea162dacad554
Guilherme <guilhermesborges23@gmail.com>
2026-09-01 20:07:35 -0300

    style: melhora layout da página principal

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-TweenMax.to("#img", 10, { rotation: 360, ease: Linear.easeNone, repeat: -1 })
+// Animação GSAP da Cabeça Girando
+const headTween = gsap.to("#img", {
+    rotation: 360,
+    duration: 8,
+    ease: "none",
+    repeat: -1
+});
+
+// Funções interativas de controle
+function setSpeed(speed) {
+    headTween.timeScale(speed);
+}
+
+function toggleAnimation() {
+    if (headTween.paused()) {
+        headTween.play();
+    } else {
+        headTween.pause();
+    }
+}
